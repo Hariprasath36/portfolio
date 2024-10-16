df0323ebca40b740d4b19f47c7ea21500e038db7
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-10-16 16:41:02 +0530

    adding seo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { sitemapPlugin } from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemapPlugin({
+      hostname: 'https://itshariprasath.vercel.app', // Your site's domain
+      // Optional: Customize the sitemap further
+      routes: [
+        '/',
+        '/about',   // Add paths based on your routes
+        '/projects',
+        '/contact',
+      ],
+    }),
+  ],
+});
