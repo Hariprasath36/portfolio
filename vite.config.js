bd7cf20095b03b6879dbdf215e0d964b361516ed
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-10-16 17:02:17 +0530

    adding seo

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { sitemapPlugin } from 'vite-plugin-sitemap';
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    sitemapPlugin({
-      hostname: 'https://itshariprasath.vercel.app', // Your site's domain
+    sitemap({
+      hostname: 'https://itshariprasath.vercel.app',
       routes: [
         '/',
-        '/about',    // Ensure these routes exist in your app
+        '/about',
         '/projects',
         '/contact',
       ],
-      // Optional: Specify output directory for sitemap (if not using default)
-      outDir: 'dist', // Adjust if your output directory is different
     }),
   ],
 });
